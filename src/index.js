1f7f5c502da7f4c7e2118cef641eaf633cd10815
aliveAmy <aliveAmy719@gmail.com>
2020-01-22 10:31:15 +0800

    init

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+window.renderComponent3 = () => {
+  const container = document.getElementById('container');
+  const containerDiv = document.createElement('div');
+  container.innerHTML = "";
+  containerDiv.id = "container-react";
+  container.appendChild(containerDiv);
+  ReactDOM.render(<App key={Math.random()} />, document.getElementById('container-react'));
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<App key={Math.random()} />, document.getElementById('container-react'));
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
